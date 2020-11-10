6de4fa272fe68cfc3e22ac5efdbeb01c9521e46e
brandonsie <brandonsie@gmail.com>
2020-11-10 10:30:49 -0500

    update ignore_cols handling in rpm() and zscore_bins()

--- R/rpm.R
+++ R/rpm.R
@@ -3,22 +3,22 @@
 #'
 #' @param reads Input data frame to rescale
 #' @param scale Value to which each column should be scaled. Default 1e6
-#' @param ignore.cols Column names to exclude from calculation (e.g. id column)
+#' @param ignore_cols Column names to exclude from calculation (e.g. id column)
 #'
 #' @export
 
 
-rpm <- function(reads, scale = 1e6, ignore.cols = NULL){
-  if(!is.null(ignore.cols)){
-    ignore_df <- reads[,ignore.cols]
-    reads <- reads %>% dplyr::select(-tidyselect::all_of(ignore.cols))
+rpm <- function(reads, scale = 1e6, ignore_cols = NULL){
+  if(!is.null(ignore_cols)){
+    ignore_df <- reads %>% subset(select = ignore_cols)
+    reads <- reads %>% dplyr::select(-tidyselect::all_of(ignore_cols))
 
   }
 
   rpm <- reads %>% apply(2, function(x){
     x * scale / sum(x)
   }) %>% as.data.frame
-  if(!is.null(ignore.cols)){
+  if(!is.null(ignore_cols)){
     rpm <- dplyr::bind_cols(ignore_df, rpm)
   }
 
