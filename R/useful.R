35c140405390b01f34c54952b81a3731683930ce
brandonsie <brandonsie@gmail.com>
2022-10-10 21:07:30 -0400

    added some convenience functions
    tidyr::crossing; magrittr pipe

---
+++ R/useful.R
@@ -0,0 +1,24 @@
+#' useful functions for other packages that i should remember to use more often
+#'
+#' tidyr::crossing is like base::expand.grid. all unique combinations of elements from 2 vectors
+#' @export
+
+
+crossing <- function(...){
+  tidyr::crossing(...)
+}
+
+
+#' magrittr pipe
+#' @export
+
+`%>%` <- function(){
+  magrittr::`%>%`
+
+}
+
+
+#' magrittr pipe
+#' @export
+
+`%^%` <- magrittr::`%>%`
