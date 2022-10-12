289b734e390ecacdcf7f1beee8aae81adb76234d
brandonsie <brandonsie@gmail.com>
2022-10-12 10:44:52 -0400

    Update ggplot_theming.R

--- R/ggplot_theming.R
+++ R/ggplot_theming.R
@@ -6,5 +6,6 @@ bms_theme <-
   ggplot2::theme(
     text = ggplot2::element_text(size = 15),
     strip.background = ggplot2::element_blank(),
-    strip.text = ggplot2::element_text(color = "black")
+    strip.text = ggplot2::element_text(color = "black"),
+    plot.title = ggplot2::element_text(size = 25, face = "bold", hjust = 0.5)
   )
